965fa31da737342a9a680252666956ff75391840
The_Bozgun <bozgun76@gmail.com>
2026-05-31 20:42:04 +0200

    feat: add frontend Dockerfile, fix S3 backend bucket name, add frontend to docker-compose (DN-302)

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "falkenwacht-terraform-state"
+    bucket = "falkenwacht-t6-terraform-state"
     key    = "terraform.tfstate"
     region = "eu-central-1"
   }
